a2ed960a169264625617ac4d8a698ca93ba0f190
ZaXaR <zaxar1990jem@gmail.com>
2022-06-07 16:52:37 +0300

    pre fix bootstrap

--- src/app/products/shared/productsUrl.ts
+++ src/app/products/shared/productsUrl.ts
@@ -1,5 +1,5 @@
 export class ProductsUrl {
-  static localhost = 'http://mine.in.ua';
+  static localhost = 'https://tab.mine.in.ua';
   static productsUrl = '/products';
   static baseProductsUrl = this.localhost + '/api/products';
   static baseCategoriesUrl = this.localhost + '/api/categories';
